3b4c4dcf8e32d9dc550577282021b1e9b006861e
Grey601 <grev.gr@hotmail.com>
2022-08-07 18:24:38 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int rubPerBonusMile = 20;
+        //int bonusMiles = cost / rubPerBonusMile;
+        return cost / rubPerBonusMile;
+    }
+}
